f694eedd91be6e43f9e258a85b4e97cb4ea6571d
Ivan Campos <sopmac21379@gmail.com>
2023-05-01 00:30:56 -0400

    js example updated with chat

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,33 +1,71 @@
-const { OpenAI } = require("langchain/llms/openai");
-const { RetrievalQAChain } = require("langchain/chains");
+const { ChatOpenAI } = require("langchain/chat_models/openai");
+const { RetrievalQAChain, LLMChain } = require("langchain/chains");
 const { HNSWLib } = require("langchain/vectorstores/hnswlib");
 const { OpenAIEmbeddings } = require("langchain/embeddings/openai");
 const { RecursiveCharacterTextSplitter } = require ("langchain/text_splitter");
 const fs = require("fs");
 
+const {
+  SystemMessagePromptTemplate,
+  HumanMessagePromptTemplate,
+  ChatPromptTemplate,
+} = require("langchain/prompts");
+
 const dotenv = require("dotenv");
 dotenv.config();
 
+const green = "\x1b[32m";
+const reset = "\x1b[0m";
+
+async function translateToBostonAccent() {
+  const translationPrompt = ChatPromptTemplate.fromPromptMessages([
+    SystemMessagePromptTemplate.fromTemplate(
+      "You are a helpful assistant that translates {input_language} to English Boston Accent."
+    ),
+    HumanMessagePromptTemplate.fromTemplate("{text}"),
+  ]);
+
+  const chat = new ChatOpenAI({
+    model_name:"gpt-3.5-turbo",
+  });
+
+  const chain = new LLMChain({
+    prompt: translationPrompt,
+    llm: chat,
+  });
+
+  const response = await chain.call({
+    input_language: "English",
+    text: "Do you want to visit the Harbor or Harvard Yard",
+  });
+  
+  let answer = JSON.stringify(response.text);
+  console.log(`${green}${answer}${reset}`);
+  console.log("\n");
+}
+
 async function chatWithTxt() {
-    // Initialize the LLM to use to answer the question.
-    const model = new OpenAI({});
-    const text = fs.readFileSync("../data/us-constitution.txt", "utf8");
-    const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
-    const docs = await textSplitter.createDocuments([text]);
-  
-    // Create a vector store from the documents.
-    const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
-  
-    // Create a chain that uses the OpenAI LLM and HNSWLib vector store.
-    const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
-    const res = await chain.call({
-      query: "What is the 2nd ammendment?",
-    });
+  // Initialize the LLM to use to answer the question.
+  const model = new ChatOpenAI({
+    model_name:"gpt-3.5-turbo",
+  });
+  const text = fs.readFileSync("../data/us-constitution.txt", "utf8");
+  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
+  const docs = await textSplitter.createDocuments([text]);
 
-    // Print the answer in green.
-    const green = "\x1b[32m";
-    const reset = "\x1b[0m";
-    console.log(`${green}${res.text}${reset}`);
-  };
+  // Create a vector store from the documents.
+  const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
 
+  // Create a chain that uses the OpenAI LLM and HNSWLib vector store.
+  const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
+  const res = await chain.call({
+    query: "What is the 2nd ammendment?",
+  });
+
+  // Print the answer in green.
+  let answer = res.text;
+  console.log(`${green}${answer}${reset}`);
+};
+
+translateToBostonAccent();
 chatWithTxt();
